dede6c83af1ffcb7ede3e55b8b8b5c4ec305589c
Deathshadowown <shanervanniekerk@gmail.com>
2020-10-14 19:53:49 +0200

    updated run.sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 rm -f src/*.class
 javac src/Main.java
 java src/Main
-rm -f src/*.class
+# rm -f src/*.class
+#creating jar file
+#jar -cvf myJarFile.jar src/Main.class
+#Execute the java file
+#java -cp MyJarFile.jar src/Main
